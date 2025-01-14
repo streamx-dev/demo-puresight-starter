5035938c6a09135fae7ec57ffa26ebac24d0a18c
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-01-14 10:41:32 +0100

    [DXP-1490] Fix for running container

--- scripts/run-proxy.sh
+++ scripts/run-proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker rm nginx-proxy
+docker rm -f nginx-proxy >/dev/null 2>&1
 
 echo "The following URLs are available:"
 echo " - http://puresight.127.0.0.1.nip.io"
@@ -9,7 +9,8 @@ echo " - http://ingestion.127.0.0.1.nip.io"
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 NGINX_CONF="${SCRIPT_DIR}/proxy.conf"
-docker run --rm --name nginx-proxy -p 80:80 nginx:1.26.0
+
+docker run -d --name nginx-proxy -p 80:80 nginx:1.26.0
 # We are not mounting, because it caused problems in WSL:
 docker cp "${NGINX_CONF}" nginx-proxy:/etc/nginx/nginx.conf
 docker exec nginx-proxy nginx -s reload
