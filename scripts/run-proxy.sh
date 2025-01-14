f50641507f21e25aadd2f110e8d4113675878e1b
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-01-14 10:43:05 +0100

    [DXP-1490] Fix for running container

--- scripts/run-proxy.sh
+++ scripts/run-proxy.sh
@@ -9,7 +9,6 @@ echo " - http://ingestion.127.0.0.1.nip.io"
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 NGINX_CONF="${SCRIPT_DIR}/proxy.conf"
-
 docker run -d --name nginx-proxy -p 80:80 nginx:1.26.0
 # We are not mounting, because it caused problems in WSL:
 docker cp "${NGINX_CONF}" nginx-proxy:/etc/nginx/nginx.conf
