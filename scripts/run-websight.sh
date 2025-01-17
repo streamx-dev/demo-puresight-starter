96796c6523a91fd7d6f98eefaed1621b08c78857
Herman Ciechanowiec <herman.ciechanowiec@ds.pl>
2025-01-17 18:33:06 +0100

    DXP-1534: Fix for WSL

--- scripts/run-websight.sh
+++ scripts/run-websight.sh
@@ -2,11 +2,12 @@ IMAGE=europe-west1-docker.pkg.dev/websight-io/websight-docker-releases/websight-
 docker pull ${IMAGE}
 # Remove the old instance
 docker rm streamx-demo-sandbox-websight-cms
+MESH_NETWORK=$(docker inspect rest-ingestion --format '{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}')
 docker run --name streamx-demo-sandbox-websight-cms \
-  --add-host=host.docker.internal:host-gateway \
+  --network "${MESH_NETWORK}" \
   -p 9001:8080 \
   -p 5009:5009 \
   -e JAVA_DEBUG_PORT=5009 \
   -e STREAMX_PUBLICATION_ENABLE=true \
-  -e PURESIGHT_STREAMX_REST_INGESTION_HOST=http://host.docker.internal \
+  -e PURESIGHT_STREAMX_REST_INGESTION_HOST=http://rest-ingestion:8080 \
   ${IMAGE}
