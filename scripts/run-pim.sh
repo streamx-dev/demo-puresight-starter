6b0c1520c25e2f32d79d05b5a3d71e0bb40d3421
Herman Ciechanowiec <herman.ciechanowiec@ds.pl>
2025-01-17 19:24:21 +0100

    DXP-1534: Fix for WSL (#8)

--- scripts/run-pim.sh
+++ scripts/run-pim.sh
@@ -2,9 +2,10 @@ IMAGE=ghcr.io/streamx-dev/demo-puresight-pim/puresight-pim:0.0.6-jvm
 docker pull ${IMAGE}
 # Remove the old instance
 docker rm streamx-demo-sandbox-pim
+MESH_NETWORK=$(docker inspect rest-ingestion --format '{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}')
 docker run --name streamx-demo-sandbox-pim \
-  --add-host=host.docker.internal:host-gateway \
+  --network "${MESH_NETWORK}" \
   -p 9000:8080 \
-  -e STREAMX_URL=http://host.docker.internal \
+  -e STREAMX_URL=http://rest-ingestion:8080 \
   -e QUARKUS_HTTP_AUTH_FORM_TIMEOUT=PT1M \
   ${IMAGE}
